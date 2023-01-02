1b32c205eefa409b736f73ce9838ef989d53e545
antonvit1 <antonbntu@gmail.com>
2023-01-02 16:47:01 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let strN = String(Math.abs(n));
+    let strLength = strN.length;
+    let a = "";
+    while (strLength > 0) {
+        a += strN[strLength - 1];
+        strLength -= 1;
+    }
+    return Number(a);
+};
